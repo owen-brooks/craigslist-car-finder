caf211d83e402bdf46aa372b763c6fa0c93ba3b3
obrook213 <owen_brooks@comcast.com>
2018-08-13 15:10:29 -0400

    add city scraper

---
+++ CityScraper.py
@@ -0,0 +1,39 @@
+import json
+import urllib.request
+from bs4 import BeautifulSoup
+
+cityURL = 'https://www.craigslist.org/about/sites'
+request = urllib.request.Request(cityURL)
+response = urllib.request.urlopen(request)
+
+
+page = BeautifulSoup(response.read().decode('utf-8'), "html.parser")
+USSection = page.find("div", {"class": "colmask"})
+
+
+def listStates():
+    stateList = []
+    for i in range(0, 51):
+        state = USSection.find_all("h4")[i].text
+        stateList.append(state)
+    return stateList
+
+
+def scrapeCarLinks():
+    # Find links for car search in all regions
+    # Returns: dict of states and links
+
+    data = {}
+    # Remove territories
+    stateList = USSection.find_all("h4")[:-1]
+
+    for idx, state in enumerate(stateList):
+        regionHTML = USSection.find_all("ul")[idx].find_all("a")
+        regionList = []
+        for ii in regionHTML:
+            regionList.append(ii['href'] + 'search/cta?query=')
+        data[state.text] = regionList
+    return data
+
+
+print(scrapeCarLinks())
